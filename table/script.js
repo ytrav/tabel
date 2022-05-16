0b7cec41166e353bf8fed7bc9c25473325de1edd
Anton <withertrav394@gmail.com>
2022-05-16 13:41:35 +0200

    xml progress, use test() function to debug

---
+++ table/script.js
@@ -0,0 +1,22 @@
+// info header
+
+var info1 = document.getElementById("info1"),
+    info2 = document.getElementById("info2"),
+    info3 = document.getElementById("info3"),
+    info4 = document.getElementById("info4"),
+    info5 = document.getElementById("info5"),
+    info6 = document.getElementById("info6"),
+    info7 = document.getElementById("info7"),
+    info8 = document.getElementById("info8"),
+    info9 = document.getElementById("info9"),
+    info10 = document.getElementById("info10"),
+    info11 = document.getElementById("info11"),
+    info12 = document.getElementById("info12");
+
+
+function test(info, value) {
+    info.innerHTML = value;
+    return;
+}
+
+
